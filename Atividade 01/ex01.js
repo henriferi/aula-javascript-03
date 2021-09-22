91d0f9d938535c2493484f81d862db0e473394d0
Henrique <henriquefernandes.gouveia@gmail.com>
2021-09-22 13:49:35 -0300

    aula3-js

---
+++ Atividade 01/ex01.js
@@ -0,0 +1,15 @@
+//parte 1
+var idade = 20;
+if (idade >= 18) {
+    console.log("Que legal! Você já pode tirar carteira de habilitação.");
+} else {
+    console.log("Poxa, que pena! Você ainda não pode ter a carteira de habilitação.")
+}
+
+//parte 2
+var ensinoMedio = true;
+if (ensinoMedio) {
+    console.log("Poxa, que legal!");
+} else {
+    console.log("Falta pouco! Logo você termina.");
+}
---
+++ Atividade 03/ex03.js
@@ -0,0 +1,26 @@
+//parte 1
+let x =5;
+let y = 7;
+if (x < y) {
+    console.log("Olá mundo!");
+} else if (x > y) {
+    console.log("Mundo olá!");
+} else {
+    console.log("Adeus!");
+}
+
+//parte 2
+let fruta = "laranja";
+switch (fruta) {
+    case "banana":
+        console.log("O filme é Os minions.");
+        break;
+    case "laranja":
+        console.log("O filme é Laraja mecânica.");
+        break;
+    case "maçã":
+        console.log("o filme é Branca de neve.");
+        break;
+    default:
+        console.log("O cinema tá fechado.");
+}
